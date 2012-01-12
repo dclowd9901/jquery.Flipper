38e37b9dcfd9515da1cfa2f81607c15bbe10d82d
dclowd9901 <david.ddrew@gmail.com>
2012-01-11 17:22:11 -0700

    Moved Underscore code out of its own file, and reduced its content to necessary components.

--- time.php
+++ time.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <head>
-	<script type="text/javascript" src="js/underscore.js"></script>		
 	<script type="text/javascript" src="js/jquery.js"></script>
-	<script type="text/javascript" src="js/fx.js"></script>	
 	<script type="text/javascript" src="js/jquery.flipper.js"></script>
 	<script type="text/javascript" src="js/demo.js"></script>
 	<link type="text/css" href="css/flipper.css" rel="stylesheet" />
